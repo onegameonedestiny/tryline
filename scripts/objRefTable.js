41e24a29c9e1de52cd756bbc9728799fdc596a69
onegameonedestiny <153502552+onegameonedestiny@users.noreply.github.com>
2025-10-24 20:27:36 +0800

    Add files via upload

--- scripts/objRefTable.js
+++ scripts/objRefTable.js
@@ -1,23 +1,39 @@
 const C3 = self.C3;
 self.C3_GetObjectRefTable = function () {
 	return [
-		C3.Plugins.Text,
+		C3.Plugins.Geolocation,
 		C3.Plugins.Button,
-		C3.Plugins.System.Acts.SetVar,
-		C3.Plugins.Text.Acts.SetText
+		C3.Plugins.Text,
+		C3.Plugins.Clipboard,
+		C3.Plugins.LocalStorage,
+		C3.Plugins.Button.Cnds.OnClicked,
+		C3.Plugins.Geolocation.Acts.RequestLocation,
+		C3.Plugins.Geolocation.Cnds.OnUpdate,
+		C3.Plugins.Text.Acts.SetText,
+		C3.Plugins.Geolocation.Exps.Longitude,
+		C3.Plugins.Geolocation.Exps.Latitude,
+		C3.Plugins.Clipboard.Acts.CopyText,
+		C3.Plugins.LocalStorage.Acts.GetItem,
+		C3.Plugins.LocalStorage.Cnds.OnItemGet,
+		C3.Plugins.LocalStorage.Exps.ItemValue
 	];
 };
 self.C3_JsPropNameTable = [
-	{Text: 0},
+	{地理位置: 0},
 	{按鈕: 0},
+	{文本: 0},
+	{剪貼板: 0},
 	{按鈕2: 0},
-	{LineUserID: 0},
-	{LiffStatus: 0},
-	{UserID_Param: 0}
+	{文本2: 0},
+	{本地存儲: 0}
 ];
 
 self.InstanceType = {
-	Text: class extends self.ITextInstance {},
+	地理位置: class extends self.IInstance {},
 	按鈕: class extends self.IButtonInstance {},
-	按鈕2: class extends self.IButtonInstance {}
+	文本: class extends self.ITextInstance {},
+	剪貼板: class extends self.IInstance {},
+	按鈕2: class extends self.IButtonInstance {},
+	文本2: class extends self.ITextInstance {},
+	本地存儲: class extends self.IInstance {}
 }
